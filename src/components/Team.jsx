8e0b82e281fd78ec7df4240d46345a60996ce03d
God'swill Sekav Apya <godswillapya@gmail.com>
2024-12-22 23:45:59 +0100

    Team Section Build

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,16 +2,27 @@ import React from "react";
 
 const Team = () => {
   return (
-    <div className="bg-aboutBlack relative">
-      <div className="mx-auto max-w-screen-2xl px-28 flex">
+    <div className="bg-aboutBlack pb-28">
+      <div className="mx-auto max-w-screen-2xl px-28 flex justify-between gap-5">
         <div>
-          <p>Our Team</p>
-          <h1>
-            Meet the dedicated professionals who bring your vision to life
+          <p className="text-[#8e8c8c] font-bold">Our Team</p>
+          <h1 className="text-[#fefefe] font-bold text-[28px] inline">
+            Our Dedicated Team <br /> Members
           </h1>
         </div>
-        <div></div>
-        <div></div>
+        <div className="relative">
+          <img src="/public/images/team-1.webp" alt="Gideon-Faj" className="w-64 rounded-lg"/>
+          <div className="absolute bottom-0">
+            <p className="text-[#fefefe]">Gideon-Faj Owoeye</p>
+            <p className="text-[#6d6d6d]">Head Innovator</p>
+          </div>
+        </div>
+        <div>
+          <img src="/public/images/team-2.webp" alt="Adebare Amos" className="w-64 rounded-lg"/> 
+        </div>
+        <div>
+          <img src="/public/images/team-1.webp" alt="Gideon-Faj" className="w-64 rounded-lg"/>
+        </div>
       </div>
     </div>
   );
