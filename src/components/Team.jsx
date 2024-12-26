4a291238e4ed42156930a0ec8f54f6ec0fa3411c
God'swill Sekav Apya <godswillapya@gmail.com>
2024-12-26 07:20:50 +0100

    Team Section Further Build

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -10,19 +10,17 @@ const Team = () => {
             Our Dedicated Team <br /> Members
           </h1>
         </div>
-        <div className="relative">
-          <img src="/public/images/team-1.webp" alt="Gideon-Faj" className="w-64 rounded-lg"/>
-          <div className="absolute bottom-0">
-            <p className="text-[#fefefe]">Gideon-Faj Owoeye</p>
-            <p className="text-[#6d6d6d]">Head Innovator</p>
+        <div className="relative overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-r from-black via-black to-black/50 opacity-50"></div>
+          <img src="/public/images/team-1.webp" alt="Gideon-Faj" className="w-64 rounded-lg transition-transform duration-300 hover:scale-110"/>
+          <div className="w-full text-center absolute bottom-5 z-30 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <p className="text-[#fefefe] text-xl font-bold">Gideon-Faj Owoeye</p>
+            <p className="text-[#222121] text-lg font-black">Head Innovator</p>
           </div>
         </div>
-        <div>
+        <div className="relative">
+          <div className="absolute inset-0 bg-gradient-to-r from-black via-black to-black/50 opacity-50"></div>
           <img src="/public/images/team-2.webp" alt="Adebare Amos" className="w-64 rounded-lg"/> 
-          <div className="absolute bottom-0">
-            <p className="text-[#fefefe]">Gideon-Faj Owoeye</p>
-            <p className="text-[#6d6d6d]">Head Innovator</p>
-          </div>
         </div>
         <div>
           <img src="/public/images/team-1.webp" alt="Gideon-Faj" className="w-64 rounded-lg"/>
